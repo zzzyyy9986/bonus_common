85262a1422fc56c922eac6e80a7fd35ee7d69454
zahar <zzzyyy589@gmail.com>
2024-04-20 00:23:36 +0300

    first item

---
+++ interfaces/NalogItem.ts
@@ -0,0 +1,10 @@
+export interface NalogItem {
+  nds: number;
+  sum: number;
+  name: string;
+  price: number;
+  quantity: number;
+  paymentType: number;
+  productType: number;
+  itemsQuantityMeasure: number;
+}
